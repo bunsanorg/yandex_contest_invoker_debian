3fa5ec69423c4a62e3983f3491272de2004aa4a0
Aleksey Filippov <sarum9in@gmail.com>
2015-05-25 03:02:40 +0200

    Updated build scripts.

--- config.sh
+++ config.sh
@@ -1,24 +1,8 @@
-extlib_prefix=bunsan_
-lib_prefix=yandex_contest_
-extlibs=(cmake testing common)
-libs=(common system invoker invoker_compat_common invoker_compat_jni invoker_flowctl_pipectl)
-repo=git://github.com/sarum9in
 lxc_pc=/usr/share/pkgconfig/lxc.pc
-
-alllibs=()
-for i in "${extlibs[@]}"
-do
-    alllibs+=("$extlib_prefix$i")
-done
-for i in "${libs[@]}"
-do
-    alllibs+=("$lib_prefix$i")
-done
-
 invoker_compat_jni_version="1.0.0-BUILD-SNAPSHOT"
 
 MFLAGS=()
-CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.8")
+CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.9" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.9")
 
 if [[ -f user-config.sh ]]
 then
---
+++ system-config.cmake
@@ -0,0 +1,3 @@
+set(ENABLE_TESTS OFF)
+set(CMAKE_BUILD_TYPE Release)
+set(CMAKE_INSTALL_PREFIX /usr)
