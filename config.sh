b70c8e47d7363c6df1c87e1250dc5e49be89d72e
Aleksey Filippov <sarum9in@gmail.com>
2015-06-04 23:28:16 +0400

    Removed lxc package from travis-ci configuration.

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@ lxc_pc=/usr/share/pkgconfig/lxc.pc
 invoker_compat_jni_version="1.0.0-BUILD-SNAPSHOT"
 
 MFLAGS=()
-CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.9" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.9")
+#CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.9" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.9")
 
 if [[ -f user-config.sh ]]
 then
